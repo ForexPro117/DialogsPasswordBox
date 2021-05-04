03074d2dd8cdc2b7108e16bbea1bfedf31565813
Роман Миниханов <roma.minihanowa@mail.ru>
2021-05-04 15:57:57 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogsPasswordBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PasswordChange win1 = new PasswordChange();
+
+        private string password = "qwerty";
+        public string Password { get => password; set => password = value; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            passwordBox.Focus();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            win1.Owner = this;
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            win1.ShowDialog();
+            Password = passwordBox.Password;
+            button1.IsEnabled = true;
+            passwordBox.Focus();
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox.Password == Password)
+                button1.IsEnabled = true;
+            else
+                button1.IsEnabled = false;
+
+        }
+
+
+    }
+}
---
+++ PasswordChange.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DialogsPasswordBox
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordChange.xaml
+    /// </summary>
+    public partial class PasswordChange : Window
+    {
+        public PasswordChange()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            (Owner.FindName("passwordBox") as PasswordBox).Password = PassBox2.Password;
+            Close();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible)
+            {
+                PassBox1.Focus();
+                PassBox1.Password = (Owner.FindName("passwordBox") as PasswordBox).Password;
+                PassBox2.Password = (Owner.FindName("passwordBox") as PasswordBox).Password;
+            }
+        }
+
+
+
+
+        private void PassBox2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PassBox1.Password != PassBox2.Password)
+                Ok.IsEnabled = false;
+            else
+                Ok.IsEnabled = true;
+        }
+
+        private void PassBox1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PassBox1.Password != PassBox2.Password)
+                Ok.IsEnabled = false;
+            else
+                Ok.IsEnabled = true;
+        }
+    }
+}
